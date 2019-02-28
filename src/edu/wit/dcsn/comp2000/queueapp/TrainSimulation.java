86d07453afb8669b7b02311588f30d23fe2ed735
Kai M. Arsenault <arsenaultk4@wit.edu>
2019-02-28 12:28:39 -0500

    First Commit

---
+++ src/edu/wit/dcsn/comp2000/queueapp/TrainSimulation.java
@@ -0,0 +1,38 @@
+/*
+ * Dave Rosenberg 
+ * Comp 2000 - Data Structures 
+ * Lab 3: Queue application - Train Simulation 
+ * Spring, 2019
+ * 
+ * Usage restrictions:
+ * 
+ * You may use this code for exploration, experimentation, and furthering your
+ * learning for this course. You may not use this code for any other
+ * assignments, in my course or elsewhere, without explicit permission, in
+ * advance, from myself (and the instructor of any other course). Further, you
+ * may not post or otherwise share this code with anyone other than current
+ * students in my sections of this course. Violation of these usage restrictions
+ * will be considered a violation of the Wentworth Institute of Technology
+ * Academic Honesty Policy.
+ */
+
+package edu.wit.dcsn.comp2000.queueapp;
+
+
+/**
+ * @author Your Name
+ * @version 1.0.0
+ */
+public class TrainSimulation
+	{
+
+	/**
+	 * @param args -unused-
+	 */
+	public static void main( String[] args )
+		{
+		// TODO Auto-generated method stub
+
+		}
+
+	}
