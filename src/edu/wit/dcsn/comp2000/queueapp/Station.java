b771a77efda8537f233e011789eae5aac2ddc23a
Rivnak, Michael <rivnakm@wit.edu>
2019-03-17 12:55:24 -0400

    fixed getPassengers method to not get more passengers than are available

--- src/edu/wit/dcsn/comp2000/queueapp/Station.java
+++ src/edu/wit/dcsn/comp2000/queueapp/Station.java
@@ -132,12 +132,13 @@ public final class Station
 	 * @return an array of passengers to be put on a train
 	 */
 	public Passenger[] getPassengers(Direction direction, int quantity) {
-	    Passenger[] output = new Passenger[quantity];
-	    for (int i = 0; i < quantity; i++) {
+		int getQuantity = Math.max(quantity, platforms.get(direction).size());
+	    Passenger[] output = new Passenger[getQuantity];
+	    for (int i = 0; i < getQuantity; i++) {
 	        output[i] = platforms.get(direction).remove();
         }
 	    Logger.write(String.format("%d passenger(s) removed from Station %s travelling %s%n",
-				quantity,
+				getQuantity,
 				this.toString(),
 				direction.toString()
 		));
