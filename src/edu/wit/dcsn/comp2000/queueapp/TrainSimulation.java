55291610586d430e43f5f4f6ca52c15282bbfd42
Ernest Shedden <48100899+sheddene@users.noreply.github.com>
2019-03-20 21:18:28 -0400

    added a disclaimer for the max number of trains supported
    The train simulation will not move the trains if the number of trains is greater than 2n-1 where n is the number of positions (including stations) on the train route

--- src/edu/wit/dcsn/comp2000/queueapp/TrainSimulation.java
+++ src/edu/wit/dcsn/comp2000/queueapp/TrainSimulation.java
@@ -64,8 +64,10 @@ public class TrainSimulation {
 		int[] theStationSpecs = config.getStations();
 		Configuration.PairedLimit[] thePassengerSpecs = config.getPassengers();
 
-		System.out.printf("Using configurations:%n\t%-20s\t: %s%n\t%-20s\t: %s%n", "Stations",
-				Arrays.toString(theStationSpecs), "Passengers", Arrays.toString(thePassengerSpecs));
+		System.out.printf("Using configurations:%n\t%-20s\t: %s%n\t%-20s\t: %s%n\t%-20s\t: %s%n\n", "Stations",
+				Arrays.toString(theStationSpecs), "Passengers", Arrays.toString(thePassengerSpecs), "Route", config.getRoute());
+		int trainLimit = 2 * config.getRoute().length - 1;
+		System.out.printf("This train route supports a maximum of " + trainLimit + " trains to remain functional\n\n");
 
 		// create a pseudo-random number generator instance
 		Random pseudoRandom = new Random(config.getSeed());
